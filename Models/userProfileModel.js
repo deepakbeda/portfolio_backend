06764169e4a4140b51a36843a6eea380bca2c043
Deepak <deepak.beda@gmail.com>
2023-03-12 17:20:26 +0530

    code updated new api created

--- Models/userProfileModel.js
+++ Models/userProfileModel.js
@@ -1,5 +1,14 @@
 mongoose = require("mongoose");
 
+const single_title = new mongoose.Schema({
+  title: {
+    type: "String",
+  },
+  icon: {
+    type: "String",
+  },
+});
+
 const userProfile = new mongoose.Schema({
   name: {
     type: "String",
@@ -17,9 +26,7 @@ const userProfile = new mongoose.Schema({
   overview: {
     type: "String",
   },
-  more_titles: {
-    type: "String",
-  },
+  more_titles: [single_title],
 });
 
 userProfiles = mongoose.model("userProfile", userProfile);
